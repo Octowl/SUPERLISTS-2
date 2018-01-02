21e27d58fe6140d13e54d378318071afc1652cd1
Octowl <alsaff1987@gmail.com>
2018-01-02 21:54:24 +0300

    Replace remaining views with CBGVs

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
-from django.views.generic import FormView
+from django.views.generic import FormView, CreateView, DetailView
 
 from lists.models import Item, List
 from lists.forms import ItemForm, ExistingListItemForm
@@ -11,23 +11,21 @@ class HomePageView(FormView):
     form_class = ItemForm
 
 
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    form = ExistingListItemForm(for_list=list_)
+class ViewAndAddToList(DetailView, CreateView):
+    model = List
+    template_name = 'list.html'
+    form_class = ExistingListItemForm
 
-    if request.method == 'POST':
-        form = ExistingListItemForm(for_list=list_, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(list_)
-    return render(request, 'list.html', {'list': list_, "form": form})
+    def get_form(self):
+        self.object = self.get_object()
+        return self.form_class(for_list=self.object, data=self.request.POST)
 
 
-def new_list(request):
-    form = ItemForm(data=request.POST)
-    if form.is_valid():
+class NewListView(CreateView):
+    form_class = ItemForm
+    template_name = 'home.html'
+
+    def form_valid(self, form):
         list_ = List.objects.create()
         form.save(for_list=list_)
         return redirect(list_)
-    else:
-        return render(request, 'home.html', {"form": form})
