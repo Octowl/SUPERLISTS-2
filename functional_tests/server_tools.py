dc4db5d9b50fb6a27b59d26c9c8f971f926f2329
Octowl <alsaff1987@gmail.com>
2018-01-15 15:30:40 +0300

    Add session creation management tools

---
+++ functional_tests/server_tools.py
@@ -0,0 +1,28 @@
+from fabric.api import run
+from fabric.context_managers import settings, shell_env
+
+
+def _get_manage_dot_py(host):
+    return f'~/sites/{host}/virtualenv/bin/python ~/sites/{host}/manage.py'
+
+
+def reset_database(host):
+    manage_dot_py = _get_manage_dot_py(host)
+    with settings(host_string=f'octowl@{host}'):
+        run(f'{manage_dot_py} flush --noinput')
+
+
+def _get_server_env_vars(host):
+    pid = run(f'pgrep -n -f {host}/virtualenv/bin/gunicorn').strip()
+    env_lines = run(f'cat /proc/{pid}/environ').split('\x00')
+    current_env = dict(l.split('=') for l in env_lines if l)
+    return current_env
+
+
+def create_session_on_server(host, email):
+    manage_dot_py = _get_manage_dot_py(host)
+    with setting(host_string=f'octowl@{host}'):
+        env_vars = _get_server_env_vars(host)
+        with shell_env(**env_vars):
+            session_key = run(f'{manage_dot_py} create_session {email}')
+            return session_key.strip()
