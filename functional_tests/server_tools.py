b4c21cc8cc5674a3bc2eb5e1b3658d7e6c1486a1
Octowl <alsaff1987@gmail.com>
2018-01-15 15:48:45 +0300

    typo

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -21,7 +21,7 @@ def _get_server_env_vars(host):
 
 def create_session_on_server(host, email):
     manage_dot_py = _get_manage_dot_py(host)
-    with setting(host_string=f'octowl@{host}'):
+    with settings(host_string=f'octowl@{host}'):
         env_vars = _get_server_env_vars(host)
         with shell_env(**env_vars):
             session_key = run(f'{manage_dot_py} create_session {email}')
